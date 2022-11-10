87995a929e47dee4b05ba39afdcd6683da0a2f6a
Andy Verh <bepx@inbox.ru>
2022-11-10 20:37:01 +0600

    добавлена лямбда , реализающая исключение персонажей кому мньше 18 лет

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,11 @@
 import java.util.*;
+import java.util.function.Predicate;
+import java.util.function.UnaryOperator;
 
 public class Main {
     public static void main(String[] args) {
-        Person fedya = new Person("Федя", "Сумкн из рода сумчатых", 18);
-        Person senya = new Person("Сеня", "Семен Семенович Ганж", 17);
+        Person fedya = new Person("Федя", "Сумкн из рода сумчатых", 17);
+        Person senya = new Person("Сеня", "Семен Семенович Ганж", 16);
         Person agronom = new Person("Агроном", "Бомж Сын Агропрома", 36);
         Person logovaz = new Person("Логоваз", "Эльф Безотцовщина Трандуил", 265);
         Person pendal = new Person("Пендальф", "Серый Майер", 126);
@@ -17,28 +19,8 @@ public class Main {
         people.add(pendal);
         people.add(givi);
 
-//        PersonsSurnameLenthComparator comparatorSurname = new PersonsSurnameLenthComparator();
-//        people.sort(comparatorSurname);
-//        System.out.println(people);
+        people.removeIf(s -> s.getAge() < 18);
+        System.out.println(people);
 
-
-
-        
     }
 }
-
-
-// из удаленного класса компаратора
-//import java.util.Comparator;
-//
-//public class PersonsSurnameLenthComparator implements Comparator<Person> {
-//
-//    @Override
-//    public int compare(Person o1, Person o2) {
-//        if (o1.getSurname().equals(o2.getSurname())) {
-//            return Integer.compare(o1.getAge(), o2.getAge());
-//        }
-//        return Integer.compare(o1.countWordsSurname(o1.getSurname()), o2.countWordsSurname(o2.getSurname()));
-//    }
-//
-//}
